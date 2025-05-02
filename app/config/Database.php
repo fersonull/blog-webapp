4f71532e37d41c55df622d198083131753f9c265
Casper <culaspero@gmail.com>
2025-05-02 14:35:36 +0800

    added Database Class for connection

---
+++ app/config/Database.php
@@ -0,0 +1,30 @@
+<?php
+namespace App\Config;
+
+class Database {
+
+    private $host = 'localhost';
+    private $dbname = 'blog_db';
+    private $username = 'root';
+    private $password = '';
+    private $conn;
+
+    public function connect()
+    {
+        $this->conn = null;
+
+        $dsn = "mysql:host=$this->host;dbname=$this->dbname";
+
+        try
+        {
+            $this->conn = new \PDO($dsn, $this->username, $this->password, [
+                \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+                \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC
+            ]);
+
+            return $this->conn;
+        } catch (\PDOException $err) {
+            echo "Error connecting: " . $err->getMessage();
+        }
+     }
+ }
